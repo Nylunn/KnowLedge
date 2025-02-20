2dcf030816f2b565b6e73dbfbc5c19c0be49721e
Nylun <kawzex@outlook.fr>
2025-02-20 14:44:12 +0100

    [+]Finishing Styling register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,29 +19,20 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, ['label' => 'Adresse email'])
             ->add('username', TextType::class, [
                 'label' => 'Nom d"utilisateur'
             ])
             ->add('delivery_adress', TextType::class, [
                 'label' => 'Adresse de livraison'
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
+            ->add('password', RepeatedType::class, [
+                 'type' => PasswordType::class,
+                  'invalid_message' => 'Les deux mots de passe doivent être identiques.',
+                  'options' => ['attr' => ['class' => 'password-field']],
+                  'required' => true,
+                  'first_options'  => ['label' => 'Mot de passe'],
+                  'second_options' => ['label' => 'Confirmer le mot de passe'],
             ])
         ;
     }
