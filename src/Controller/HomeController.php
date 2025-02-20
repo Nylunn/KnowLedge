c11698e6088963224a5229a7a248cefaa811b41c
Nylun <kawzex@outlook.fr>
2025-02-20 13:24:55 +0100

    [+]Finishing style for the home page (logged)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Sweatshirt;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    
-    public function index(): Response
+   #[Route('/', name: 'app_home')]
+       public function index(ManagerRegistry $manager): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $products = $manager->getRepository(Sweatshirt::class)->findAll();
+
+        return $this->render('home/index.html.twig', ['products' => $products ]);
+
+
     }
 }
