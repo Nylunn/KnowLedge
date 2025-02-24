5bdd64f269d114760ef67b824bc5382600f3cf65
Nylun <kawzex@outlook.fr>
2025-02-24 20:47:21 +0100

    [+]Structuring the starting database

---
+++ src/Entity/Initiation.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InitiationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InitiationRepository::class)]
+class Initiation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    //Column for the title of the cursus
+
+     #[ORM\Column]
+    private ?string $title = null;
+
+
+    public function getTitle(): ?int
+    {
+        return $this->title;
+    }
+
+    //Column for the type of the cursus
+
+     #[ORM\Column]
+    private ?string $type = null;
+
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+ 
+    //Column for the price of the cursus
+
+     #[ORM\Column]
+    private ?string $price = null;
+
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+}
