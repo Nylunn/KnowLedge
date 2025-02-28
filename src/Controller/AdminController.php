398d905b64ccf901134c22c779191b8fc182f8d8
Nylun <kawzex@outlook.fr>
2025-02-28 15:40:34 +0100

    [+]Adding Admin User Gestion

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,12 +6,15 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\Formation;
 use App\Entity\Lesson;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 use App\Service\StripeService;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class AdminController extends AbstractController
@@ -39,6 +42,44 @@ final class AdminController extends AbstractController
 
 
     }
+
+
+      #[Route('/users', name: 'admin_users')]
+    public function listUsers(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/user.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/user/edit/{id}', name: 'admin_user_edit')]
+    public function editUser(User $user, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Utilisateur mis à jour avec succès');
+            return $this->redirectToRoute('admin_users');
+        }
+
+        return $this->render('admin/edit_user.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/user/delete/{id}', name: 'admin_user_delete', methods: ['POST'])]
+    public function deleteUser(User $user, EntityManagerInterface $em): Response
+    {
+        $em->remove($user);
+        $em->flush();
+        $this->addFlash('success', 'Utilisateur supprimé avec succès');
+        return $this->redirectToRoute('admin_users');
+    }
+
 
     
 
